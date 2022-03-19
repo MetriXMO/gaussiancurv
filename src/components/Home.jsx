c7b137e88440ca43a86db4777261fecb753a3d16
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-03-19 15:23:32 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { React } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { AttentionSeeker } from 'react-awesome-reveal';
 import Particles from "../components/particles";
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
