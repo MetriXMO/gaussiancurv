06986e44a16973b3382600b09876675a7d3d737d
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-04-01 00:54:48 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,17 +13,17 @@ const Home = ({ darkMode }) => {
  
 
 
-    return ( <div>
+    return ( <Particles/><div>
          
         <div id="particles-js">
-       <Particles>   
+          
        
       <div class="header">
                
       <span class="site-title">Gaussian Curvature</span>
                     <span class="site-description">GC has turned over one year, check out <FooterLink  href="https://artofproblemsolving.com/community/c5h2766985p24238194"><h5>OPMC</h5></FooterLink>  </span>   
                        </div> 
-            </Particles>
+          
               
 </div>
 
